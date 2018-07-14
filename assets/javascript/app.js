1330fef582bde292c58497f386b718c529322aa4
Chris Russell <christine.russell10@gmail.com>
2018-07-14 10:01:06 -0400

    started html & JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,25 @@
-$(function() {
-    
+$(function () {
+    var questions = ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5"];
+    var answerImage = $("img.answerImage");
+    var userAnswer;
+    var correctAnswer;
+    var currentQuestion;
+    var questionSet = [];
+    var numCorrect = 0;
+    var numIncorrect = 0;
 
+    function selectQuestions () {
 
-//DO NOT CODE BENEATH THIS LINE
+    }
+
+    function newRound(){
+        answerImage.hide();
+        numCorrect = 0;
+        numIncorrect = 0;
+        questionSet = [];
+        
+    }
+
+    newRound();
+    //DO NOT CODE BENEATH THIS LINE
 })();
